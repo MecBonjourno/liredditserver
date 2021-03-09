c17b10887f51c02911aebd4f6e33e60e217e5fd0
Guilherme Correa Zago <guilherme.zago@pampa.compasso>
2021-03-09 01:32:31 -0300

    começar o front grazadeus

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,55 @@ import { ApolloServer } from 'apollo-server-express'
 import {buildSchema} from 'type-graphql'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis"
+import { MyContext } from "./types";
+
+var cookieParser = require('cookie-parser')
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up()
     const app = express();
+
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    app.use(cookieParser())
+
+    app.get('/', function (req) {
+        // Cookies that have not been signed
+        console.log('Cookies: ', req.cookies)
+      
+        // Cookies that have been signed
+        console.log('Signed Cookies: ', req.signedCookies)
+      })
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({client: redisClient, disableTouch: true}),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+                httpOnly: true,
+                sameSite: 'lax', //csrf
+                secure: __prod__, //https 
+            },
+            saveUninitialized: false,
+            secret: "ahisfhuasfgaudshiadhsuaisudga",
+            resave: false,
+        })
+    )
     
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({em: orm.em})
+        context: ({req,res}): MyContext => ({em: orm.em, res ,req })
     })
 
     apolloServer.applyMiddleware({ app })
